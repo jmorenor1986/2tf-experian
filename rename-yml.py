d53960298209d72368ca853ae91177730f0d1d99
jmorenor1986 <johnmorenoing@gmail.com>
2020-05-17 15:14:54 +0200

    update rename

--- rename-yml.py
+++ rename-yml.py
@@ -7,4 +7,4 @@ import sys
 
 with fileinput.FileInput(sys.argv[1], inplace=True, backup='.bak') as file:
     for line in file:
-        print(line.replace('/experian-rest','/'+sys.argv[2]+'-'+sys.argv[3] ), end='')
+        print(line.replace('/experian-rest','/'+sys.argv[2]), end='')
